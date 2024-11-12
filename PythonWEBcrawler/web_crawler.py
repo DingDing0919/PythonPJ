01c7f6c728eef5a79a7b1455ae6cd1869693081a
Chien, Cheng Wei <103027232+DingDing0919@users.noreply.github.com>
2024-11-12 14:28:18 +0000

    test wev

--- PythonWEBcrawler/web_crawler.py
+++ PythonWEBcrawler/web_crawler.py
@@ -1,2 +1,17 @@
 import requests
-from requests import get
+
+"""
+# Use Get Function
+web = get('https://water.taiwanstat.com/') 
+
+# Change UTF-8
+web.encoding = 'utf-8'
+
+# Print Out
+print(web.text)
+"""
+
+# web2 
+web =requests.get('https://data.kcg.gov.tw/12345')
+if web.status_code == 404:
+    print("404 Error!!")
