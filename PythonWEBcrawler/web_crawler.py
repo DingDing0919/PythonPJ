f54659188884fce8cbeaf38f02a56e5d9f4f6acc
Chien, Cheng Wei <103027232+DingDing0919@users.noreply.github.com>
2024-11-12 14:36:44 +0000

    params

--- PythonWEBcrawler/web_crawler.py
+++ PythonWEBcrawler/web_crawler.py
@@ -10,8 +10,21 @@ web.encoding = 'utf-8'
 # Print Out
 print(web.text)
 """
-
+"""
 # web2 
 web =requests.get('https://data.kcg.gov.tw/12345')
 if web.status_code == 404:
     print("404 Error!!")
+"""
+
+# Set Num
+Params = {
+    'name':'oxxo',
+    'age':'18'
+}
+
+# Add Num
+web = requests.get('https://script.google.com/macros/s/AKfycbw5PnzwybI_VoZaHz65TpA5DYuLkxIF-HUGjJ6jRTOje0E6bVo/exec',
+                   params = Params)
+
+print(web.text)
